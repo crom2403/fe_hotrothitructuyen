3b838a1379f8d286735bb720963845ce9b8be5b3
tsondev <thanhsonrau2002@gmail.com>
2025-05-28 22:01:58 +0700

    feat/test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="text-red-500">test</div>
+  return (
+    <div className="text-red-500">
+      thanhsondev
+      <div className="bg-blue-700">hahaha</div>
+    </div>
+  )
 }
 
 export default App
