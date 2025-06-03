a4133ecf75a4daa8d8113cfb191e2254e04f60ca
TTTLevi <thientong712001@gmail.com>
2025-06-03 16:02:49 +0700

    install some initial packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button"
+
 function App() {
-  return <div className="text-red-500">test</div>
+  return <div className="text-red-500">
+    <Button className="bg-amber-300">Click me</Button>
+  </div>
 }
 
 export default App
