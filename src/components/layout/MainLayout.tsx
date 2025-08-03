acd827b117531bc1daa33fdc81facf9a0743f74f
TTTLevi <thientong712001@gmail.com>
2025-08-03 12:30:31 +0700

    fail responsive

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
         <AppSidebar />
         <div className="flex-1 flex flex-col">
           <Header />
-          <main className="flex-1 overflow-y-auto bg-gray-50 px-12 py-8">
+          <main className="flex-1 overflow-y-auto bg-gray-50 px-12 py-8 ">
             <Outlet />
           </main>
         </div>
